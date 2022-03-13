12d1fd1052be5ca67f6e01d864e330fe91157e67
YZ <charliehe0211@gmail.com>
2022-03-13 21:38:23 +0100

    d

--- RTL/control_unit.v
+++ RTL/control_unit.v
@@ -77,24 +77,24 @@ module control_unit(
          end
 
          LOAD_WORD:begin
-            alu_src   = 1'b1;
+            alu_src   = 1'b0;
             mem_2_reg = 1'b1;
             reg_write = 1'b1;
             mem_read  = 1'b1;
             mem_write = 1'b0;
             branch    = 1'b0;
-            alu_op    = ADD_OPCODE;
+            alu_op    = R_TYPE_OPCODE;
             jump      = 1'b0;
          end
 
          STORE_WORD:begin
-            alu_src   = 1'b1;
+            alu_src   = 1'b0;
             mem_2_reg = 1'b0;
             reg_write = 1'b0;
             mem_read  = 1'b0;
             mem_write = 1'b1;
             branch    = 1'b0;
-            alu_op    = ADD_OPCODE;
+            alu_op    = R_TYPE_OPCODE;
             jump      = 1'b0;
          end
 
