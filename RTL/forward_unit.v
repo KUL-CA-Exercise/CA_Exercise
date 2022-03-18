9d0a19c67c01fd088f9b62925dbfb85c0fa56154
YZ <charliehe0211@gmail.com>
2022-03-18 16:55:28 +0100

    t

--- RTL/forward_unit.v
+++ RTL/forward_unit.v
@@ -3,15 +3,17 @@ module forward_unit(
 	input wire [4:0] rs2_ex,
 	input wire [4:0] rd_mem,
 	input wire [4:0] rd_wb,
+	input wire mem_read,
+	input wire mem_2_reg,
 		
 	output reg [1:0] mux_alu_1,
 	output reg [1:0] mux_alu_2
 );
 	always @(*) begin	
-		if(rs1_ex == rd_mem || rs2_ex == rd_mem) begin
+		if((rs1_ex == rd_mem || rs2_ex == rd_mem) && mem_read) begin
 			mux_alu_1 = 2'b10;
 			mux_alu_2 = 2'b10;
-		end else if(rs1_ex == rd_wb || rs2_ex == rd_wb) begin
+		end else if((rs1_ex == rd_wb || rs2_ex == rd_wb) && mem_2_reg) begin
 			mux_alu_1 = 2'b1;
 			mux_alu_2 = 2'b1;
 		end else begin
