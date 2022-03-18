4da11ac90b1ead89245c4c7a572391cca4aa549f
YZ <charliehe0211@gmail.com>
2022-03-18 18:26:24 +0100

    MULT3: works with hard wires

--- RTL/forward_unit.v
+++ RTL/forward_unit.v
@@ -11,6 +11,7 @@ module forward_unit(
 );
 	always @(*) begin	
 		if((rs1_ex == rd_mem || rs2_ex == rd_mem)) begin
+			//todo: remove hard wire
 			mux_alu_1 = 2'b10;
 			mux_alu_2 = 2'b0;
 		end else if((rs1_ex == rd_wb || rs2_ex == rd_wb)) begin
