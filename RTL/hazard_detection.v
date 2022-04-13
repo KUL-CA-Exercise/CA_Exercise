ba377602651a2ce5b8e2a65b4f95a00a77e6a477
YZ <charliehe0211@gmail.com>
2022-04-13 23:40:10 +0200

    hazard unit test

--- RTL/hazard_detection.v
+++ RTL/hazard_detection.v
@@ -1,26 +1,23 @@
-module hazard_detection(
-    input [4:0] rs1_id,
-    input [4:0] rs2_id,
-    input [4:0] rd_ex,
-    input wire mem_read_ex,
-
-    output reg pc_w,
-    output reg pipeline_id_en,
-    output reg hazard_mux_sel
-)
-
+module hazard_detection(input [4:0] rs1_id,
+                        input [4:0] rs2_id,
+                        input [4:0] rd_ex,
+                        input wire mem_read_ex,
+                        output reg pc_w,
+                        output reg pipeline_id_en,
+                        output reg hazard_mux_sel);
+    
     always @(*) begin
-        if(mem_read_ex && (rd_ex == rs1_id || rd_ex == rs2_id)) begin
+        if (mem_read_ex && (rd_ex == rs1_id || rd_ex == rs2_id)) begin
             // Load-use hazard: stall
-            pc_w = 1'b0;
+            pc_w           = 1'b0;
             pipeline_id_en = 1'b0;
             hazard_mux_sel = 1'b1;
-        end else begin
+            end else begin
             // no hazard
-            pc_w = 1'b1;
+            pc_w           = 1'b1;
             pipeline_id_en = 1'b1;
             hazard_mux_sel = 1'b0
         end
     end
-
+    
 endmodule
