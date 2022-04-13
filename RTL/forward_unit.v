54ee58facffa6ce17b22bbf82eb22184b37c2f11
YZ <charliehe0211@gmail.com>
2022-04-13 17:36:59 +0200

    t

--- RTL/forward_unit.v
+++ RTL/forward_unit.v
@@ -8,30 +8,25 @@ module forward_unit(input wire [4:0] rs1_ex,
                     output reg [1:0] mux_alu_1,
                     output reg [1:0] mux_alu_2);
 
-always @(*) begin
-    // if (reg_write_mem) begin
-    	// EX hazard
-        if (rs1_ex == rd_mem) begin
-            mux_alu_1 = 2'b10;
-            mux_alu_2 = 2'b0;
-        end else if (rs2_ex == rd_mem) begin
-            mux_alu_1 = 2'b0;
-            mux_alu_2 = 2'b10;
-        end
-    // end else if (reg_write_wb) begin
-    	// MEM hazard
-   		else if (rs1_ex == rd_wb) begin
-   		    mux_alu_1 = 2'b1;
-   		    mux_alu_2 = 2'b0;
-   		end else if (rs2_ex == rd_wb) begin
-   		    mux_alu_1 = 2'b0;
-   		    mux_alu_2 = 2'b1;
-   		end 
-	// end 
-	else begin
-   		    mux_alu_1 = 2'b00;
-   		    mux_alu_2 = 2'b00;
-   	end
-end
+	always @(*) begin
+	    // EX hazard
+	    if (reg_write_mem && (rs1_ex == rd_mem)) begin
+	        mux_alu_1 = 2'b10;
+	        mux_alu_2 = 2'b0;
+	    end else if (reg_write_mem && (rs2_ex == rd_mem)) begin
+	        mux_alu_1 = 2'b0;
+	        mux_alu_2 = 2'b10;
+	    end else if (reg_write_wb && (rs1_ex == rd_wb)) begin
+	    	// MEM hazard
+	   		mux_alu_1 = 2'b1;
+	   		mux_alu_2 = 2'b0;
+	   	end else if (reg_write_wb && (rs2_ex == rd_wb)) begin
+	   		mux_alu_1 = 2'b0;
+	   		mux_alu_2 = 2'b1;
+	   	end else begin
+	   		mux_alu_1 = 2'b00;
+	   		mux_alu_2 = 2'b00;
+	   	end	
+	end
 
 endmodule
